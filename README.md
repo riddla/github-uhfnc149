87bc05dc0b04f8131aab678f02dfa948ca16bc46
riddla <volker.rose@gmail.com>
2026-09-05 12:15:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-uhfnc149
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/riddla/github-uhfnc149)
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright IBM Corp. 2020, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import '@carbon/web-components/es/components/data-table/table.js';
+import '@carbon/web-components/es/components/data-table/table-toolbar.js';
+import '@carbon/web-components/es/components/data-table/table-toolbar-content.js';
+import '@carbon/web-components/es/components/multi-select/multi-select.js';
+import '@carbon/web-components/es/components/multi-select/multi-select-item.js';

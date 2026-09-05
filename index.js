1b90006d1f3635d8d025396bc26db9342e1a951e
Volker Rose <v.rose@tricks.de>
2026-09-05 12:42:37 +0200

    add table components

--- index.js
+++ index.js
@@ -5,11 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import '@carbon/web-components/es/components/data-table/table.js';
-import '@carbon/web-components/es/components/data-table/table-toolbar.js';
-import '@carbon/web-components/es/components/data-table/table-toolbar-content.js';
-import '@carbon/web-components/es/components/multi-select/multi-select.js';
-import '@carbon/web-components/es/components/multi-select/multi-select-item.js';
-import '@carbon/web-components/es/components/grid/grid.js'
-import '@carbon/web-components/es/components/grid/column.js'
-import '@carbon/web-components/es/components/tile/tile.js'
+import "@carbon/web-components/es/components/data-table/table.js";
+import "@carbon/web-components/es/components/data-table/table-toolbar.js";
+import "@carbon/web-components/es/components/tile/tile.js";
+import "@carbon/web-components/es/components/data-table/table-toolbar-content.js";
+import "@carbon/web-components/es/components/data-table/table-header-row.js";
+import "@carbon/web-components/es/components/data-table/table-header-cell.js";
+import "@carbon/web-components/es/components/grid/grid.js";
+import "@carbon/web-components/es/components/grid/column.js";
+import "@carbon/web-components/es/components/multi-select/multi-select.js";
+import "@carbon/web-components/es/components/multi-select/multi-select-item.js";
